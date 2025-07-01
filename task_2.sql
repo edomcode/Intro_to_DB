b1aed90b09026a06443b927112fdfcc7c60b30cd
Eden Yehualashet <edenyehualashet27@gmail.com>
2025-07-01 05:50:26 -0700

    Fix VARCHAR sizes to match checker for Customers table

--- task_2.sql
+++ task_2.sql
@@ -25,8 +25,8 @@ CREATE TABLE IF NOT EXISTS Books (
 -- Create Customers table
 CREATE TABLE IF NOT EXISTS Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
-    customer_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
     phone VARCHAR(20),
     address TEXT
 );
